27a1eb05cd6d3a70861a29b00bc11ffde45d5a76
Anatolii Voronkin <anatoliivoronkinqa@gmail.com>
2023-09-29 16:17:08 +0300

    comment

--- conftest.py
+++ conftest.py
@@ -13,3 +13,24 @@ def set_up(page):
 
     yield page
     page.close()
+
+@pytest.fixture()
+def login_set_up(context_creation, browser):
+    context = browser.new_context(storage_state="state.json")
+    page = context.new_page()
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    page.set_default_timeout(3000)
+
+    yield page
+
+@pytest.fixture
+def go_to_new_collection_page(page):
+    # Assess - Given
+    # browser = playwright.chromium.launch(headless=False)
+    # context = browser.new_context()
+    # # Open new page
+    # page = context.new_page()
+    page.goto("/new-collection")
+    page.set_default_timeout(3000)
+
+    yield page
--- pom/home_page_elements.py
+++ pom/home_page_elements.py
@@ -1,1 +1,3 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+class HomePage:
+    celebrating_beauty_header = "text=Celebrating Beauty and Style"
+    celebrating_beauty_body = "text=playwright-practice was founded by a group of like-minded fashion devotees, dete"
